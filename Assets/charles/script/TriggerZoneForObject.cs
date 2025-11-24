f6f8d452db96797dea04db0c52ebabc19c9ae708
MUKILTEO\1725289 <charles82508@gmail.com>
2025-11-24 08:41:07 -0800

    the door opens

--- Assets/charles/script/TriggerZoneForObject.cs
+++ Assets/charles/script/TriggerZoneForObject.cs
@@ -1,17 +1,25 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class TriggerZoneForObject : MonoBehaviour
 {
     public GameObject targetObject;
-    
+    private bool dooropen=false;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (targetObject == null)
+       dooropen=true;
+    }
+    private void Update()
+    {
+        if (dooropen = true)
         {
-            Vector3 pos = targetObject.transform.position;
-            pos.y += 2f;
-            targetObject.transform.position = pos;
+            if (targetObject.transform.position.y <= 5f)
+            {
+                Vector3 pos = targetObject.transform.position;
+                pos.y += .5f;
+                targetObject.transform.position = pos;
+            }
         }
     }
 }
