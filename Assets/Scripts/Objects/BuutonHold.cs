2e9b08c70bb732ce7a2dbf2ae4aa6bad19bf29ef
MUKILTEO\1725608 <talonlee44@gmail.com>
2025-12-11 09:57:59 -0800

    Tutorial WP

---
+++ Assets/Scripts/Objects/BuutonHold.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class BuutonHold : MonoBehaviour
+{
+    public Sprite spro;
+    public Sprite spru;
+
+    private SpriteRenderer spriteRenderer;
+
+    public openDoor2 doorScript;
+
+    private void Awake()
+    {         
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Magnet"))
+        {
+            Debug.Log("Button Pressed!");
+            // Add additional button press logic here
+            spriteRenderer.sprite = spro;
+            doorScript.doorOpen = true;
+        }
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Magnet"))
+        {
+            Debug.Log("Button Released!");
+            // Add additional button release logic here
+            spriteRenderer.sprite = spru;
+            doorScript.doorOpen = false;
+        }
+    }
+}
