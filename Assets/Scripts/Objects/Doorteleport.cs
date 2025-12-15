374a41c1f8bd8b28b607c9cc338b2f84709ecc08
1725607 <1725607@mukilteo.wednet.edu>
2025-12-15 10:02:02 -0800

    Worked on main level

--- Assets/Scripts/Objects/Doorteleport.cs
+++ Assets/Scripts/Objects/Doorteleport.cs
@@ -2,13 +2,12 @@ using UnityEngine;
 
 public class Doorteleport : MonoBehaviour
 {
-    public publicGameObject playerPosition;
+    public GameObject teleportPosition;
 
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            other.transform.position = playerPosition;
-        }
+            other.transform.position = teleportPosition;
     }
 }
