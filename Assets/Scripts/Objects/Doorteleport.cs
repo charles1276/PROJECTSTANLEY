a0b3f0591b5b9a85dca5fe001b2e4c1f24a4e814
MUKILTEO\1725608 <talonlee44@gmail.com>
2025-12-15 10:20:54 -0800

    WP tutorial

--- Assets/Scripts/Objects/Doorteleport.cs
+++ Assets/Scripts/Objects/Doorteleport.cs
@@ -1,13 +1,14 @@
 using UnityEngine;
 
-public class Doorteleport : MonoBehaviour
-{
-    public GameObject teleportPosition;
+//public class Doorteleport : MonoBehaviour
+//{
+//    public GameObject teleportPosition;
 
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            other.transform.position = teleportPosition;
-    }
-}
+//    void OnCollisionEnter2D(Collision2D other)
+//    {
+//        if (other.gameObject.CompareTag("Player"))
+//        {
+//            other.transform.position = teleportPosition;
+//        }
+//    }
+//}
