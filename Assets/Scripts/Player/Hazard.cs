9586e28e203d61c8bcec743ee3f06f18dc8730e1
MUKILTEO\1725375 <azenosnothere@gmail.com>
2026-01-05 08:51:02 -0800

    updated hazard script

--- Assets/Scripts/Player/Hazard.cs
+++ Assets/Scripts/Player/Hazard.cs
@@ -7,7 +7,12 @@ public class Hazard : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            // set player to the last checkpoint position
+            other.transform.position = Checkpoint.lastCheckpointPosition;
+            other.gameObject.GetComponent<InventoryManager>().inventorySlots = Checkpoint.playerInventory;
+
+            // Reload the current scene to respawn the player at the last checkpoint
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 }
